51579d7fe41fd80dfb4efd860ec9d1721ca5ca93
margreen94 <margreen@margreen-pc.wireless-1x.unc.edu>
2020-03-04 10:43:56 -0500

    attempt grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import "./btn-group.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HELLOOO
-        </a>
-      </header>
-    </div>
+    <body>
+      {/* <script src="./grid.js"></script> */}
+      {/* <var></var> */}
+      <td>
+        <th aria-label="1">1</th>
+        <th aria-label="2">2</th>
+        <th aria-label="3">3</th>
+        <th aria-label="4">4</th>
+        <th aria-label="5">5</th>
+        <th aria-label="6">6</th>
+        <th aria-label="7">7</th>
+        <th aria-label="8">8</th>
+        <th aria-label="9">9</th>
+        <th aria-label="10">10</th>
+      </td>
+      <br></br>
+      <div class="btn-group">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>10</button>
+      </div>
+    </body>
   );
 }
 
---
+++ src/grid.js
@@ -0,0 +1,4 @@
+var i;
+for (i = 0; i < 10; i++) {
+  "<button>" + i + "</button>";
+}
