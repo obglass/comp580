21fcc68260a649267789a1e9daee9acf15098d4c
Olivia Glass <oliviabglass@gmail.com>
2020-04-19 21:42:26 -0400

    fixing error

---
+++ precache-manifest.f6e43d4bbd444c776eeed5c2c57b290a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3ddc98564b384d8f153da289bbf816",
+    "url": "/comp580/index.html"
+  },
+  {
+    "revision": "a5862f217edf40e8e8b7",
+    "url": "/comp580/static/css/main.1de20d5f.chunk.css"
+  },
+  {
+    "revision": "7d747bd5f1a6172e0693",
+    "url": "/comp580/static/js/2.c9ccf965.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/comp580/static/js/2.c9ccf965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5862f217edf40e8e8b7",
+    "url": "/comp580/static/js/main.4ae14aba.chunk.js"
+  },
+  {
+    "revision": "910eff8cd726cc639d2e",
+    "url": "/comp580/static/js/runtime-main.2ebc74a0.js"
+  }
+]);
