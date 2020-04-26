f80d5fc7aaf7e3707f62f3a994c85197d1fdf550
Olivia Glass <oliviabglass@gmail.com>
2020-04-26 14:17:47 -0400

    i really hope this works

---
+++ precache-manifest.35ff0ecc773d880b9a9b753406ffc4e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c40acaf6e2528581298376f2e786149",
+    "url": "/comp580/index.html"
+  },
+  {
+    "revision": "375f16b58bc8f342385d",
+    "url": "/comp580/static/css/main.d3f63255.chunk.css"
+  },
+  {
+    "revision": "6a0c4137473c2d59dcf1",
+    "url": "/comp580/static/js/2.509d7243.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/comp580/static/js/2.509d7243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375f16b58bc8f342385d",
+    "url": "/comp580/static/js/main.0a044259.chunk.js"
+  },
+  {
+    "revision": "910eff8cd726cc639d2e",
+    "url": "/comp580/static/js/runtime-main.2ebc74a0.js"
+  }
+]);
